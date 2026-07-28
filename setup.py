176aa3628c6cbc7a96f734f5fd535aabe3bf4856
Fira Fatmasiefa <58095915+firafsiefa@users.noreply.github.com>
2026-07-28 17:40:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='pines_rimas_analysis_toolkit',
     version='1.0.0',
-    author='Patrick Tamburo',
-    author_email='tamburop@bu.edu',
-    description='Analysis pipeline for PINES.',
+    author='Fira Fatmasiefa',
+    author_email='ffsiefa@bu.edu',
+    description='Analysis pipeline for LDT-RIMAS.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/firafsiefa/pines_rimas_analysis_toolkit',
-    license='MIT',
+    license='BU',
     packages=['pines_rimas_analysis_toolkit'],
     install_requires=['julian'],
 )
