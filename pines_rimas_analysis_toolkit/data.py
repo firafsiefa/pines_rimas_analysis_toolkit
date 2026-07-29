1e507337c3392ed51be77c97cb377608f9a88187
Fira Fatmasiefa <58095915+firafsiefa@users.noreply.github.com>
2026-07-29 13:56:36 -0400

    Update data.py

--- pines_rimas_analysis_toolkit/data.py
+++ pines_rimas_analysis_toolkit/data.py
@@ -8,7 +8,7 @@ from astropy.stats import histogram, sigma_clipped_stats
 from scipy.stats import sigmaclip
 
 from photutils.background import Background2D, MedianBackground
-from datetime
+import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
