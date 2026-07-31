8fffa1910598fbedc6495b197e9b09f8856c0be7
Fira Fatmasiefa <58095915+firafsiefa@users.noreply.github.com>
2026-07-31 13:54:26 -0400

    .

--- pines_rimas_analysis_toolkit/analysis.py
+++ pines_rimas_analysis_toolkit/analysis.py
@@ -1821,6 +1821,11 @@ def weighted_lightcurve(short_name, filter='J', phot_type='aper', convergence_th
         df = pd.read_csv(phot_file)
         df.columns = df.keys().str.strip()
 
+        ref_names = np.delete(ref_names, no_ref)
+        num_refs = len(ref_names)
+        ref_source_x = np.delete(ref_source_x, no_ref)
+        ref_source_y = np.delete(ref_source_y, no_ref)
+        
         #Restrict arrays to filter_inds
         full_times = np.array(df['Time BJD TDB'])
         full_file_list = np.array(df['Filename'])
