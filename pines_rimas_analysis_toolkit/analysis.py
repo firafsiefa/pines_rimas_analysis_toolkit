6522dc514761910f54a1ddb8ddc95fe674a399e6
Fira Fatmasiefa <58095915+firafsiefa@users.noreply.github.com>
2026-07-31 14:16:56 -0400

    .

--- pines_rimas_analysis_toolkit/analysis.py
+++ pines_rimas_analysis_toolkit/analysis.py
@@ -12,6 +12,7 @@ from natsort import natsorted
 import os
 from sklearn import linear_model
 import matplotlib.dates as mdates
+from matplotlib.offsetbox import AnchoredText
 from pines_rimas_analysis_toolkit.utils import pines_dir_check, short_name_creator, pines_log_reader, get_source_names
 from pines_rimas_analysis_toolkit.pwv import pwv_spectrum_time_series
 from astropy.io import fits
