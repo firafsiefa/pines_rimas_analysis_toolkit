9ec756c2ed478a44b2e06909651ee299e6c32fb2
Fira Fatmasiefa <58095915+firafsiefa@users.noreply.github.com>
2026-07-31 17:34:49 -0400

    .

--- pines_rimas_analysis_toolkit/photometry.py
+++ pines_rimas_analysis_toolkit/photometry.py
@@ -2676,3 +2676,268 @@ def centroider_FF(short_name, sources, filter='', output_plots=False, restore=Fa
     print('centroider runtime: {:.2f} minutes.'.format((time.time()-t1)/60))
     print('')
     return centroid_df
+
+def manual_ref_star_chooser(short_name, profile_data, positions=[], restore=False, source_detect_plot=False, force_output_path=''):
+    """Chooses suitable reference stars for a target in a specified source_detect_image.
+
+    :param short_name: the short name for the target
+    :type short_name: str
+    :param profile_data: dictionary containing parameters for source_detection output from utils.profile_reader()
+    :type profile_data: dict
+    :type restore: bool, optional
+    :param source_detect_plot: whenther or not to plot all detected sources, defaults to False
+    :type source_detect_plot: bool, optional
+    :param force_output_path: top-level 'force output path', used if you want to use a folder other than ~/Documents/PINES_analysis_toolkit/, defaults to ''
+    :type force_output_path: str, optional
+    :raises ValueError: If the measured seeing in your selected source_detect_image is Nan
+    :raises ValueError: If the measured x/y shift in your selected_source_detect_image is > 20 pixels
+    :return: Saves a plot of the target/detected reference stars to the object's 'sources' directory. Saves .csv of target/reference pixel positions in the object's 'sources' directory.
+    :rtype: plot/csv
+    """
+
+
+    plt.ion()
+
+    source_detect_image = profile_data['source_detect_image']
+    source_detect_filter = profile_data['source_detect_filter']
+    guess_position=(profile_data['guess_position_x'],profile_data['guess_position_y'])
+    exclude_lower_left=profile_data['exclude_lower_left']
+    dimness_tolerance=profile_data['dimness_tolerance']
+    brightness_tolerance=profile_data['brightness_tolerance']
+    distance_from_target=profile_data['distance_from_target']
+    non_linear_limit=profile_data['non_linear_limit']
+    edge_tolerance=profile_data['edge_tolerance']
+
+    # Get your local PINES directory
+    if force_output_path != '':
+        pines_path = force_output_path
+    else:
+        pines_path = pines_dir_check()
+
+    if restore:
+        output_df = pd.read_csv(
+            pines_path/('Objects/'+short_name+'/sources/target_and_references_source_detection.csv'))
+        print('')
+        print('Restoring ref_star_chooser output from {}.'.format(
+            pines_path/('Objects/'+short_name+'/sources/target_and_references_source_detection.csv')))
+        print('')
+        plt.ioff()
+        return output_df
+    
+    source_detect_image_path = pines_path/('Objects/'+short_name+'/reduced/'+source_detect_filter+'/'+source_detect_image)
+
+    # Set path to source directory.
+    source_dir = pines_path/('Objects/'+short_name+'/sources/'+source_detect_filter+'/')
+
+    source_frame = source_detect_image_path.name.split('_')[0]+'.fits'
+    log_name = source_frame.split('.')[0]+'_log.txt'
+    log = pines_log_reader(pines_path/('Logs/'+log_name))
+    source_frame_ind = np.where(log['Filename'] == source_frame)[0][0]
+    extra_x_shift = log['X shift'][source_frame_ind]
+    extra_y_shift = log['Y shift'][source_frame_ind]
+    source_detect_seeing = float(log['X seeing'][source_frame_ind])
+
+    # Make sure the seeing value isn't a NaN.
+    if np.isnan(source_detect_seeing):
+        raise RuntimeError('Seeing value = NaN in log. Try a different source_detect_image in {}.profile.'.format(short_name.lower().replace(' ','')))
+
+    if (float(extra_x_shift) > 20) or (float(extra_y_shift) > 20):
+        raise RuntimeError('Measured x or y shift > 20 pixels. Try a different source_detect_image in {}.profile.'.format(short_name.lower().replace(' ','')))
+
+    extra_shift_path = (source_dir/'extra_shifts.txt')
+    with open(extra_shift_path, 'w') as file:
+        file.write(str(extra_x_shift)+' '+str(extra_y_shift))
+
+
+    # Detect sources in the image.
+    #sources = detect_sources(source_detect_image_path, source_detect_seeing, positions,
+     #                        edge_tolerance, plot=source_detect_plot, thresh=3.0)
+    sources = pd.DataFrame({'x_center':positions[:,0], 'y_center': positions[:,1]})
+
+    # Identify the target in the image using guess_position.
+    target_id = target_finder(sources, guess_position)
+
+    # Now determine ids of suitable reference stars.
+    image = fits.open(source_detect_image_path)[0].data
+
+    # Interpolate nans if any in image.
+    kernel = Gaussian2DKernel(x_stddev=0.5)
+    image = interpolate_replace_nans(image, kernel)
+
+    suitable_ref_ids = []
+    bad_ref_ids = [target_id]
+    # Get a value for the brightness of the target with a radius_check aperture. We don't want reference stars dimmer than dimness_tolerance * targ_flux_estimates.
+    target_ap = CircularAperture(
+        (sources['x_center'][target_id], sources['y_center'][target_id]), r=7)
+    target_an = CircularAnnulus(
+        (sources['x_center'][target_id], sources['y_center'][target_id]), r_in=12, r_out=30)
+    mask = target_an.to_mask(method='center')
+    an_data = mask.multiply(image)
+    an_data_1d = an_data[an_data != 0]
+    vals, lower, upper = sigmaclip(an_data_1d)
+    bg_estimate = np.median(vals)
+    targ_flux_estimate = aperture_photometry(
+        image, target_ap)['aperture_sum'] - bg_estimate * target_ap.area
+
+    for i in range(len(sources)):
+        if i != target_id:
+            potential_ref_loc = (sources['x_center'][i], sources['y_center'][i])
+            ap = CircularAperture(potential_ref_loc, r=7)
+            an = CircularAnnulus(potential_ref_loc, r_in=12, r_out=30)
+            mask = an.to_mask(method='center')
+            an_data = mask.multiply(image)
+            an_data_1d = an_data[an_data != 0]
+            vals, lower, upper = sigmaclip(an_data_1d)
+            bg_estimate = np.median(vals)
+
+            pixels = ap.to_mask()
+            sub_image = pixels.cutout(image)
+
+            dists = []
+            for j in range(len(sources)):
+                dists.append(np.sqrt((sources['x_center'][i]-sources['x_center'][j])**2+(
+                    sources['y_center'][i]-sources['y_center'][j])**2))
+            dists = np.array(dists)
+
+            # Check if potential ref is near non-linear regime.
+            non_linear_flag = len(
+                np.where(sub_image > non_linear_limit)[0]) == 0
+            # Check if potential ref is bright enough.
+            dimness_flag = ((aperture_photometry(image, ap)[
+                            'aperture_sum'] - bg_estimate * ap.area) > dimness_tolerance*targ_flux_estimate)[0]
+            brightness_flag = ((aperture_photometry(image, ap)[
+                               'aperture_sum'] - bg_estimate * ap.area) < brightness_tolerance*targ_flux_estimate)[0]
+            #Cut sources that are too close to another
+            closeness_flag = (len(np.where(dists[np.where(dists != 0)] < 15)[0]) == 0)
+            proximity_flag = (np.sqrt((sources['x_center'][target_id]-sources['x_center'][i])**2+(
+                sources['y_center'][target_id]-sources['y_center'][i])**2) < distance_from_target)
+            if exclude_lower_left:
+                if (sources['x_center'][i] < 512) & (sources['y_center'][i] < 512):
+                    lower_left_flag = False
+                else:
+                    lower_left_flag = True
+            else:
+                lower_left_flag = True
+
+            if (non_linear_flag) & (dimness_flag) & (brightness_flag) & (closeness_flag) & (proximity_flag) & (lower_left_flag):
+                suitable_ref_ids.append(i)
+            else:
+                bad_ref_ids.append(i)
+
+    #ax.plot(sources['xcenter'][suitable_ref_ids], sources['ycenter'][suitable_ref_ids],'yx')
+    print('Found {} suitable reference stars.'.format(len(suitable_ref_ids)))
+    print('')
+
+    if len(suitable_ref_ids) < 2:
+        print('Not enough reference stars found. Try loosening reference star criteria.')
+        plt.ioff()
+        return
+
+    target = sources.iloc[[target_id]].reset_index(
+        drop=True).drop(columns=['id'])
+    suitable_refs = sources.drop(bad_ref_ids).reset_index(
+        drop=True).drop(columns=['id'])
+
+
+    output_dict = {'Name':[], 'Source Detect X':[], 'Source Detect Y':[]}
+    output_dict['Name'].append(short_name)
+    output_dict['Source Detect X'].append(sources['x_center'][target_id])
+    output_dict['Source Detect Y'].append(sources['y_center'][target_id])
+
+
+    for i in range(len(suitable_refs)):
+        output_dict['Name'].append('Reference '+str(i+1))
+        output_dict['Source Detect X'].append(sources['x_center'][suitable_ref_ids[i]])
+        output_dict['Source Detect Y'].append(sources['y_center'][suitable_ref_ids[i]])
+
+    output_df = pd.DataFrame(output_dict)
+
+    stats = sigma_clipped_stats(image)
+    fig, ax = plt.subplots(figsize=(10, 9))
+    ax.set_aspect('equal')
+    im = ax.imshow(
+        image, vmin=stats[1], vmax=stats[1]+7*stats[2], origin='lower', cmap='Greys')
+    cax = fig.add_axes([0.87, 0.15, 0.035, 0.7])
+    fig.colorbar(im, cax=cax, orientation='vertical', label='ADU')
+    ax.set_title(source_detect_image_path.name)
+    ax.plot(output_df['Source Detect X'][0], output_df['Source Detect Y'][0],
+            marker='o', color='m', mew=2, ms=12, ls='', mfc='None', label='Target')
+    # Plot selected references
+    for i in range(1, len(output_df)):
+        ax.plot(output_df['Source Detect X'][i], output_df['Source Detect Y'][i],
+                marker='o', color='b', mew=2, ms=12, ls='', mfc='None', label='Reference')
+        ax.text(output_df['Source Detect X'][i]+7,
+                output_df['Source Detect Y'][i]+5, str(i), fontsize=14, color='r')
+    # Plot detected sources
+    for i in range(len(sources)):
+        ax.plot(sources['x_center'], sources['y_center'], 'rx',
+                label='Detected sources', alpha=0.6, ms=3)
+
+    handles, labels = ax.get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    ax.legend(by_label.values(), by_label.keys(), bbox_to_anchor=(1.1, 1.1))
+    plt.show()
+
+    # Sometimes, the source detection returns things that are clearly not reference stars.
+    # Allow the user to remove them here.
+    ans = input('Enter IDs of references to remove, separated by commas (e.g.: 1,4,8,14).\nIf none to remove, hit enter:  ')
+    if ans != '':
+        ans = ans.split(',')
+        ref_ids_to_remove = []
+        for i in range(len(ans)):
+            ref_ids_to_remove.append(int(ans[i]))
+
+        # Reverse sort the list.
+        ref_ids_to_remove = np.sort(ref_ids_to_remove)[::-1]
+        # Drop those references from output_df
+        for i in ref_ids_to_remove:
+            output_df = output_df.drop(index=i)
+        output_df = output_df.reset_index(drop=True)
+        # Rename the remaining references.
+        for i in range(1, len(output_df)):
+            name = output_df['Name'][i]
+            if int(name.split(' ')[1]) != i:
+                name = 'Reference '+str(i)
+                output_df['Name'][i] = name
+        # Replot everything.
+        ax.cla()
+        im = ax.imshow(image, origin='lower',
+                       vmin=stats[1], vmax=stats[1]+7*stats[2], cmap='Greys')
+        cax = fig.add_axes([0.87, 0.15, 0.035, 0.7])
+        fig.colorbar(im, cax=cax, orientation='vertical', label='ADU')
+        ax.set_title(source_detect_image_path.name)
+        ax.plot(output_df['Source Detect X'][0], output_df['Source Detect Y'][0],
+                marker='o', color='m', mew=2, ms=12, ls='', mfc='None', label='Target')
+        for i in range(1, len(output_df)):
+            ax.plot(output_df['Source Detect X'][i], output_df['Source Detect Y'][i],
+                    marker='o', color='b', mew=2, ms=12, ls='', mfc='None', label='Reference')
+            ax.text(output_df['Source Detect X'][i]+7,
+                    output_df['Source Detect Y'][i]+5, str(i), color='r', fontsize=14)
+        handles, labels = ax.get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+        ax.legend(by_label.values(), by_label.keys(),
+                  bbox_to_anchor=(1.1, 1.1))
+    plt.show()
+
+    ans = input('Happy with reference star selection? y/n: ')
+    if ans == 'y':
+        print('')
+        print('Saving target/reference info to {}.'.format(source_dir /
+              'target_and_references_source_detection.csv'))
+        output_df.to_csv(
+            source_dir/'target_and_references_source_detection.csv', index=False)
+        print('')
+        print('Saving target and references image to {}.'.format(
+            source_dir/('target_and_refs.png')))
+        plt.savefig(source_dir/('target_and_refs.png'))
+        plt.ioff()
+        plt.close('all')
+
+        return output_df
+    elif ans == 'n':
+        plt.ioff()
+        raise ValueError(
+            'Try changing arguments of ref_star_chooser or detect_sources and try again.')
+    else:
+        plt.ioff()
+        return
